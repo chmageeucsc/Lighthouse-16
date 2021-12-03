305b765ef793085717978f9c0192b19954170258
“Shaina <shainamish@gmail.com>
2021-12-03 04:53:06 -0800

    Button Text Displaying
    Text for buttons displays on canvas

--- Lighthouse 16/Assets/Scripts/Button.cs
+++ Lighthouse 16/Assets/Scripts/Button.cs
@@ -6,32 +6,41 @@ using UnityEngine.EventSystems;
 
 public class Button : MonoBehaviour
 {
-    public Text Textfield;
+    public Text Textfield1;
+    public Text Textfield2;
+    public Text Textfield3;
     public int num1 = 0;
     public int num2 = 0;
     public int num3 = 0;
 
-    public void SetText(string text){
-        Textfield.text = text;
-    }
-
     public void OnClicked(Button button)
     {
-        //print(button.name);
+        // find buttons
+        Textfield1 = GameObject.Find("Cross Text").GetComponent<Text> ();
+        Textfield2 = GameObject.Find("Wing Text").GetComponent<Text> ();
+        Textfield3 = GameObject.Find("Rock Text").GetComponent<Text> ();
 
         // check which button is pressed
         if(button.name == "1 Up"){
+            // add to total
             num1 = NumAdd(num1);
+            // send to text box
+            Textfield1.text = num1.ToString();
         } else if(button.name == "1 Down"){
             num1 = NumSub(num1);
+            Textfield1.text = num1.ToString();
         } else if(button.name == "2 Up"){
             num2 = NumAdd(num2);
+            Textfield2.text = num2.ToString();
         } else if(button.name == "2 Down"){
             num2 = NumSub(num2);
+            Textfield2.text = num2.ToString();
         } else if(button.name == "3 Up"){
             num3 = NumAdd(num3);
+            Textfield3.text = num3.ToString();
         } else if(button.name == "3 Down"){
             num3 = NumSub(num3);
+            Textfield3.text = num3.ToString();
         } 
     }
 
@@ -39,21 +48,18 @@ public class Button : MonoBehaviour
         if(num < 9){
             num = (num + 1);
         }
-        print("add num: " +  num);
         return num;
     }
 
     public int NumSub(int num){
         if(num > 0){
-            print("in bitch");
             num = (num - 1);
         }
-        print("sub num: " +  num);
         return num;
     }
 
     void Update()
     {
-        //EventSystem.current.currentSelectedGameObject.name; 
+        
     }
 }
